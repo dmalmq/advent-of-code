af664d879cd49ec9a390b12dcc7e345d58be2f28
Daniel Malmqvist <dnlmalmqvist@gmail.com>
2023-07-06 19:47:04 +0900

    finished day 4

---
+++ 2022/day-4/day4b.js
@@ -0,0 +1,33 @@
+import * as fs from 'fs';
+
+const input = fs.readFileSync('input.txt', 'utf8')
+  .split('\n')
+
+input.pop()
+
+
+
+console.log(input)
+
+let sum = 0
+
+for (let i = 0; i < input.length; i++) {
+
+  let values = input[i].split(",");
+  const firstTwo = (values[0].split('-'))
+  const secondTwo = (values[1].split('-'))
+
+
+  const firstMin = parseInt(firstTwo[0])
+  const firstMax = parseInt(firstTwo[1])
+  const secondMin = parseInt(secondTwo[0])
+  const secondMax = parseInt(secondTwo[1])
+
+
+  if ((firstMax >= secondMin && firstMax <= secondMax) || (secondMax >= firstMin && secondMax <= firstMax)) {
+    sum++
+    console.log(`${firstTwo} ${secondTwo} ${sum}`)
+  }
+}
+
+console.log(sum)
